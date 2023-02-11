8f1a28a0859391ec677a4325ffc358f5dd171df9
kunevi <rotherexram@gmail.com>
2023-02-11 10:10:26 +0100

    i don't even know

--- src/cpu.rs
+++ src/cpu.rs
@@ -44,13 +44,17 @@ impl Cpu {
 
     pub fn execute(&mut self) {
         loop {
-            let instruction = self.fetch_and_increment();
+            let instruction = self.fetch_byte();
             match instruction {
                 LDA_IM => self.lda_im(),
                 LDA_ABS => self.lda_abs(),
                 LDA_ABSX => self.lda_absx(),
+                LDA_ABSY => self.lda_absy(),
                 LDA_ZP => self.lda_zp(),
                 LDA_ZPX => self.lda_zpx(),
+                LDA_ZPXI => self.lda_zpxi(),
+                LDA_ZPYI => self.lda_zpyi(),
+                JSR => self.jsr(),
                 NOP => self.nop(),
                 _ => {
                     panic!("Unknown instruction: {}", instruction);
@@ -59,18 +63,21 @@ impl Cpu {
         }
     }
 
-    // fetch a single byte from zero page memory
-    fn fetch_zero_page(&mut self, address: usize) -> u8 {
-        if self.pc as usize > mem::MAX_MEM {
-            panic!("PC exceeds max memory allocated {}", mem::MAX_MEM);
-        }
-
-        self.mem.mem[address]
-    }
-
     // fetch a single byte from memory
     fn fetch_memory(&mut self, address: usize) -> u8 {
         self.mem.mem[address]
+    }
+
+    // read a single byte from memory
+    fn read_byte(&mut self, address: usize) -> u8 {
+        self.mem.mem[address]
+    }
+
+    // fetch word from memory
+    fn read_word(&mut self, address: usize) -> u16 {
+        let mut data = self.fetch_memory(address) as u16;
+        data |= u16::from(self.fetch_memory(address + 1)) << 8;
+        data
     }
 
     // fetch word from memory
@@ -82,15 +89,13 @@ impl Cpu {
         data
     }
 
-    fn fetch(&mut self, address: usize) -> u8 {
-        if self.pc as usize > mem::MAX_MEM {
-            panic!("PC exceeds max memory allocated {}", mem::MAX_MEM);
-        }
-
-        self.mem.mem[address]
+    // write word to memory at address
+    fn write_word(&mut self, address: usize, data: u16) {
+        self.mem.mem[address] = (data & 0xFF) as u8;
+        self.mem.mem[address + 1] = (data >> 8) as u8;
     }
 
-    fn fetch_and_increment(&mut self) -> u8 {
+    fn fetch_byte(&mut self) -> u8 {
         if self.pc as usize > mem::MAX_MEM {
             panic!("PC exceeds max memory allocated {}", mem::MAX_MEM);
         }
@@ -104,7 +109,7 @@ impl Cpu {
 
     // load accumulator immediate
     fn lda_im(&mut self) {
-        self.a = self.fetch_and_increment();
+        self.a = self.fetch_byte();
         self.lda_set_flags();
     }
 
@@ -121,26 +126,60 @@ impl Cpu {
         self.lda_set_flags();
     }
 
+    // load accumulator absolute, y index
+    fn lda_absy(&mut self) {
+        let address = self.fetch_word();
+        self.a = self.fetch_memory((address + self.y as u16) as usize);
+        self.lda_set_flags();
+    }
+
     // load accumulator zero page
     fn lda_zp(&mut self) {
-        let address = self.fetch_and_increment();
-        self.a = self.fetch_zero_page(address as usize);
+        let address = self.fetch_byte();
+        self.a = self.read_byte(address as usize);
         self.lda_set_flags();
     }
 
     // load accumulator zero page, x index
     fn lda_zpx(&mut self) {
-        let address = self.fetch_and_increment();
-        self.a = self.fetch_zero_page((address + self.x) as usize);
+        let address = self.fetch_byte();
+        self.a = self.read_byte((address + self.x) as usize);
+        self.lda_set_flags();
+    }
+
+    // load accumulator zero page, x index indirect
+    fn lda_zpxi(&mut self) {
+        let indirect_address = self.fetch_byte() + self.x;
+        // & 0xFF will wrap to start of zero page if overflow
+        self.a = self.read_byte((indirect_address & 0xFF) as usize);
+        self.lda_set_flags();
+    }
+
+    // load accumulator zero page indirect y indexed
+    fn lda_zpyi(&mut self) {
+        let zero_page_address = self.fetch_byte() + self.y;
+
+        let effective_address = self.read_word(zero_page_address as usize);
+        let effective_address_y = effective_address + self.y as u16;
+
+        self.a = self.fetch_memory(effective_address_y as usize);
         self.lda_set_flags();
     }
 
     // set zero and negative flags whenever an LDA instruction is executed
     fn lda_set_flags(&mut self) {
         // set zero flag
-        self.p.set(ProcStat::Z, bool::from(self.a == 0));
+        self.p.set(ProcStat::Z, self.a == 0);
         self.p
-            .set(ProcStat::N, bool::from((self.a & 0b10000000) > 0));
+            .set(ProcStat::N, (self.a & 0b10000000) > 0);
+    }
+
+    // jump to a subroutine by pushing the pc onto the stack and modifying the pc
+    fn jsr(&mut self) {
+        let sub_address = self.fetch_word();
+        self.write_word(self.sp as usize, self.pc - 1);
+        self.sp -= 2;
+        self.pc = sub_address;
     }
 
     // no-op
--- src/op_codes.rs
+++ src/op_codes.rs
@@ -2,11 +2,19 @@
 pub const LDA_IM: u8 = 0xA9;
 // load accumulator absolute
 pub const LDA_ABS: u8 = 0xAD;
-// load accumulator absolute, x index
+// load accumulator absolute x indexed
 pub const LDA_ABSX: u8 = 0xBD;
+// load accumulator absolute y indexed
+pub const LDA_ABSY: u8 = 0xB9;
 // load accumulator zero page
 pub const LDA_ZP: u8 = 0xA5;
 // load accumulator zero page, x index
 pub const LDA_ZPX: u8 = 0xB5;
+// load accumulator zero page x indexed indirect
+pub const LDA_ZPXI: u8 = 0xA1;
+// load accumulator zero page y indexed indirect
+pub const LDA_ZPYI: u8 = 0xB1;
 // no-op
 pub const NOP: u8 = 0xEA;
+// jump subroutine
+pub const JSR: u8 = 0x20;
